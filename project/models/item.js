2e740ca659ba63f9470b74ad7ecfaefa74253e69
xxihyun0830 <xxihyun0830@gmail.com>
2019-12-09 00:14:18 +0900

    view>item ..end

--- project/models/item.js
+++ project/models/item.js
@@ -5,9 +5,10 @@ const Schema = mongoose.Schema;
 var schema = new Schema({
     user_id : {type:Schema.Types.ObjectId, ref : 'User'},
     title : {type: String, trim:true, required:true},
+    place : {type: String, trim:true, required:true},
     content :{type: String, trim:true, required:true},
-    keyword : [String],
-    
+    course :{type: String, trim:true },
+        
     // option : 다양한 정렬 : 인기순, 후기순, 가격순, 신상품순, 투어의 종류별(종류를 키워드로 분류할 예정임) 
     numLikes: {type: Number, default:0},
     numComments : {type: Number, default : 0},
